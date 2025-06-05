92f3420cca1e1426e3d95790c0402659ab815020
abhainarayanan <abhaynarayanan11@gmail.com>
2025-06-05 14:46:52 +0530

    new file added

---
+++ sampe_project1/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace sampe_project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num > 0)
+            {
+
+                Console.WriteLine("its Positive number!");
+            }
+            else
+            {
+                Console.WriteLine("its Negative number!");
+            }
+
+            Console.Read();
+        }
+
+    }
+}
